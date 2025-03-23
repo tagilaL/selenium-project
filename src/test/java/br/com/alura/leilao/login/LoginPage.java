a77497209ae524407332506c14655f5f7bae441f
Tágila Lima <tagila.lima96@gmail.com>
2025-03-23 10:31:51 -0300

    Incluindo testes unitarios do Login

---
+++ src/test/java/br/com/alura/leilao/login/LoginPage.java
@@ -0,0 +1,50 @@
+package br.com.alura.leilao.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class LoginPage {
+    private static final String URL_LOGIN = "http://localhost:8080/login";
+
+    private WebDriver browser;
+
+    public LoginPage() {
+        System.setProperty("webdriver.edge.driver","drivers/msedgedriver.exe");
+        this.browser = new EdgeDriver();
+        this.browser.navigate().to(URL_LOGIN);
+    }
+
+    public void preencherFormularioDeLogin(String username, String password) {
+        browser.findElement(By.id("username")).sendKeys(username);
+        browser.findElement(By.id("password")).sendKeys(password);
+    }
+
+    public void efetuarLogin() {
+        browser.findElement(By.id("login-form")).submit();
+    }
+
+    public String getNomeUsuarioLogado() {
+        try {
+            return browser.findElement(By.id("usuario-logado")).getText();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+    }
+
+    public boolean isPaginaAtual() {
+        return browser.getCurrentUrl().contains(URL_LOGIN);
+    }
+
+    public boolean isMensagemDeLoginInvalidoVisivel() {
+        return browser.getPageSource().contains("Usuário e senha inválidos");
+    }
+
+    public void fechar() {
+        this.browser.quit();
+    }
+
+
+}
---
+++ src/test/java/leiloes/LeiloesPage.java
@@ -0,0 +1,4 @@
+package leiloes;
+
+public class LeiloesPage {
+}
